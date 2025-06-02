71eb31756802317e4fae925f559abc9f3c791f54
Serugii <luhanskyi.serhii@student.uzhnu.edu.ua>
2025-06-02 23:30:27 +0300

    Finish project for coursework

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,8 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, only: [ :dashboard ]
 
   def index
-    # доступна для всіх
   end
 
   def dashboard
-    # доступна лише для авторизованих користувачів
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
-  layout 'application'
+  layout "application"
 end
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  layout 'application'
+  layout "application"
 end
